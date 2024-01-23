09b72217e11c7d716548715985cbcc3c71d97c5c
Navin <navinchinnasamy@gmail.com>
2024-01-23 12:36:49 +0530

    Basic Chatbit Plugin
    Basic Chatbit Plugin created with webpack

--- webpack.config.js
+++ webpack.config.js
@@ -1,21 +1,24 @@
+// webpack.config.js
 const path = require('path');
 
 module.exports = {
-	entry: './src/index.js',
-	output: {
-		filename: 'chatbot.min.js',
-		path: path.resolve(__dirname, 'dist'),
-	},
-	module: {
-		rules: [
-			{
-				test: /\.html$/,
-				use: ['html-loader'],
-			},
-			{
-				test: /\.css$/,
-				use: ['style-loader', 'css-loader'],
-			},
-		],
-	},
+    mode: 'development',
+    devtool: 'source-map',
+    entry: './src/index.js', // Your entry file
+    output: {
+        filename: 'chatbot-plugin.js',
+        path: path.resolve(__dirname, 'dist'),
+        library: {
+            name: 'ChatbotPlugin',
+            type: 'umd',
+        },
+    },
+    module: {
+        rules: [
+            {
+                test: /\.css$/,
+                use: ['style-loader', 'css-loader'],
+            },
+        ],
+    },
 };
